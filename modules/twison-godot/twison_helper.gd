c281a162af00d96444b3a52c7d08ada08d3ba643
Vladislav Slobodenyuk <toastergithubmail@gmail.com>
2020-01-04 20:30:14 +0900

    Tags update.

--- modules/twison-godot/twison_helper.gd
+++ modules/twison-godot/twison_helper.gd
@@ -6,25 +6,61 @@ extends Object
 var passages = {}
 var data = {}
 
+# Tag db is a Dictionary with tag strings as keys, and Arrays of passage pids as values
+var _tag_db_enabled = false
+var tag_db = {}
+
 # Parses the file for passages
 # If this script was already filled with data, it will be lost.
-
+# --------------------------------------------------------------------------------
 # linkFilter is a function that accepts a RegExMatch containing link,
 # and returns with what this link should be replaced.
 # For more info take a look at two example filters provided, and the documentation. 
-func parse_file(filePath: String, linkFilter: FuncRef = null):
+# --------------------------------------------------------------------------------
+# construct_tag_db : If you pass 'true' to this method, the tag db will be constructed
+# while parsing the file. It allows faster tag operations, if there are many of them.
+# I honestly don't know why you might wanna disable it, but the option is there.
+func parse_file(filePath: String, \
+				linkFilter: FuncRef = null, \
+				construct_tag_db = true):
 	data = {}
 	passages = {}
+	
 	data = _load_file(filePath)
 	_extract_passages()
+	
+	if (construct_tag_db):
+		_tag_db_enabled = true
+		_construct_tag_db()
+	
 	if (linkFilter == null):
 		linkFilter = funcref(self, "link_filter_erase")
+
 	_filter_links(linkFilter)
 
 func _extract_passages():
 	for passage in data["passages"]:
 		var pid = int(passage["pid"])
 		passages[pid] = passage
+
+# Constructs tag db. If you pass true as an argument,
+# instead of assigning constructed db to global var,
+# this funtion will return it.
+func _construct_tag_db(return_it: bool = false):
+	var tags = {}
+	for pid in passages:
+		var passage = passages[pid]
+		
+		if (passage.has("tags")):
+			for tag in passage["tags"]:
+				if tags.has(tag):
+					tags[tag].append(pid)
+				else:
+					tags[tag] = [pid]
+	if (return_it):
+		return tags
+	else:
+		tag_db = tags
 
 func _filter_links(linkFilter: FuncRef):
 	# \[\[(.+?(?=\]\]))\]\]
@@ -35,6 +71,7 @@ func _filter_links(linkFilter: FuncRef):
 	var text
 	for passage in passages:
 		text = passages[passage]["text"]
+		
 		for result in reg.search_all(text):
 			var newLink = linkFilter.call_func(result)
 			text = text.replace(result.get_string(), newLink)
@@ -56,7 +93,7 @@ func _load_file(filePath: String):
 
 	return data
 
-# Identifyes name and link in string match.
+# Identifies name and link in string match.
 # It is best not to send link with braces here,
 # as they will not be stripped
 static func identify_link(link: String):
@@ -122,6 +159,33 @@ func get_passage_names():
 
 func has_passage(pid: int):
 	return passages.has(pid)
+
+# Returns an Array with all tag strings.
+func get_all_tags():
+	var result = []
+	if (_tag_db_enabled):
+		for tag in tag_db:
+			result.append(tag)
+	else:
+		var tags = _construct_tag_db(true)
+		for tag in tags:
+			result.append(tag)
+	return result
+
+# Returns all passage pids which are tagged with this tag.
+# If the tag does not exist, empty array is returned.
+func get_passages_tagged_with(tag: String):
+	if (_tag_db_enabled):
+		if (tag_db.has(tag)):
+			return tag_db[tag]
+		else:
+			return []
+	else:
+		var tags = _construct_tag_db(true)
+		if (tags.has(tag)):
+			return tags[tag]
+		else:
+			return []
 
 # Returns a pid
 # Returns -1 if starting node not found
